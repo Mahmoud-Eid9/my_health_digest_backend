bf7f3255a7c1694bcb2680f5e2cff7f603ca50ab
mahmoud-eid9 <mahmoudeid947@gmail.com>
2022-05-21 20:31:09 +0200

    final

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -26,9 +26,12 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
+  if (!process.env.MONGO_URL) {
+    throw new Error('DB connection string must be defined');
+  }
 
   try {
-    await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+    await mongoose.connect(process.env.MONGO_URL);
     console.log('connected to MongoDb');
   } catch (err) {
     console.log(err);
