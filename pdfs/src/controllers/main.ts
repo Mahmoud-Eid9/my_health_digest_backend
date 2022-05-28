be3714d86b72ba413d92ff3901e5aada0fc7285b
mahmoud-eid9 <mahmoudeid947@gmail.com>
2022-05-28 22:24:05 +0200

    updated localhost to 10.0.2.2

--- pdfs/src/controllers/main.ts
+++ pdfs/src/controllers/main.ts
@@ -1,22 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-
-
-const arrangePdfs = (category: string) => { return new Promise((resolve, reject) => {
-  const directoryPath = path.resolve(__dirname, `../public/${category}`);
-  let pdfs = {};
-  try {
-    const filenames = fs.readdirSync(directoryPath);
-    filenames.forEach((file: string) => {
-      pdfs = { ...pdfs, [file]: `http://localhost/api/pdfs/static/${category}/${file}` };
-    });
-    resolve(pdfs);
-  } catch (err) {
-    reject('Server Error');
-  }
-});
-}
-
+const arrangePdfs = (category: string) => {
+  return new Promise((resolve, reject) => {
+    const directoryPath = path.resolve(__dirname, `../public/${category}`);
+    let pdfs = {};
+    try {
+      const filenames = fs.readdirSync(directoryPath);
+      filenames.forEach((file: string) => {
+        pdfs = { ...pdfs, [file]: `http://10.0.2.2/api/pdfs/static/${category}/${file}` };
+      });
+      resolve(pdfs);
+    } catch (err) {
+      reject('Server Error');
+    }
+  });
+};
 
 export { arrangePdfs as arrangePdfs };
