6c7fc382d30e75c4e93fdd13dde04f79e3e725a6
mahmoud-eid9 <mahmoudeid947@gmail.com>
2022-09-12 20:08:58 +0200

    hah

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import { requireAuth } from '@myhealthdigest/auth-middleware';
+import { BadRequestError, requireAuth } from '@myhealthdigest/auth-middleware';
 
 import { currentUser } from '@myhealthdigest/auth-middleware';
 import { isRegularExpressionLiteral } from 'typescript';
@@ -7,7 +7,14 @@ import { isRegularExpressionLiteral } from 'typescript';
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, requireAuth, (req, res) => {
-  res.send({ currentUser: req.currentUser || null });
+  try {
+    if(!currentUser){
+      throw new BadRequestError("No Such User")
+    }
+    res.send({ ...req.currentUser || null }); 
+  } catch (error) {
+    res.send("No Such User")
+  }
   //in this case if currentUser is not logged in it will be
   //undefined so the or null to make sure the output is unified
 });
