bac3f16355b519894b238529c939935eb355ee08
Mahmoud-Eid9 <62315812+Mahmoud-Eid9@users.noreply.github.com>
2022-09-29 21:55:31 +0200

    Testing Auth Deployment

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
+  console.log("Sartinggg");
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined!');
   }
